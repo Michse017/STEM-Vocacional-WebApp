90154cdef73bf27baee85ab1217c669a3b401293
Michse017 <severichecastellano@gmail.com>
2025-11-11 11:33:35 -0500

    correccion rutas login

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -190,7 +190,7 @@ export default function LandingPage() {
             <CardDescription>
               Gestión de cuestionarios: crear, editar y publicar versiones, y definir un cuestionario primario.
             </CardDescription>
-            <CardButton to="/admin/login" color="#10B981">
+            <CardButton to="login" color="#10B981">
               Abrir portal
             </CardButton>
           </PublicCard>
@@ -365,7 +365,7 @@ export default function LandingPage() {
           <FooterLinks>
             <FooterLink to="/login">Acceso Estudiantes</FooterLink>
             <FooterSeparator>•</FooterSeparator>
-            <FooterLink to="/admin/login">Acceso Administradores</FooterLink>
+            <FooterLink to="/login">Acceso Administradores</FooterLink>
           </FooterLinks>
           <Copyright>© 2025 OrientaSTEM. Todos los derechos reservados.</Copyright>
         </FooterContent>
